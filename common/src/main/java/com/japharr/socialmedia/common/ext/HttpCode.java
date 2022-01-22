22797c692011526276dcd1307cd525866412a6ab
Jelil Adesina <jtutoff@gmail.com>
2022-01-22 18:55:51 +0100

    httpCode status

---
+++ common/src/main/java/com/japharr/socialmedia/common/ext/HttpCode.java
@@ -0,0 +1,8 @@
+package com.japharr.socialmedia.common.ext;
+
+public interface HttpCode {
+  int HTTP_OK = 200;
+  int HTTP_BAD_REQUEST = 400;
+  int HTTP_NOT_FOUND = 404;
+  int HTTP_SERVER_ERROR = 500;
+}
